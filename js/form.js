7b6386581535cceb3de3a3aa80f05c9fd0b0404f
Fabricio Kubota <fabriciok@gmail.com>
2017-12-29 16:19:21 -0200

    Separando funcionalidades

--- js/form.js
+++ js/form.js
@@ -1,39 +1,3 @@
-var titulo = document.querySelector(".titulo");
-console.log(titulo);
-titulo.textContent = "Aparecida Nutricionista";
-
-
-var pacientes = document.querySelectorAll(".paciente");
-
-for(var i=0; i<pacientes.length; i++){
-
-    var trPaciente = pacientes[i];
-
-    var peso = trPaciente.querySelector(".info-peso").textContent;
-    var altura = trPaciente.querySelector(".info-altura").textContent;
-    var trIMC = trPaciente.querySelector(".info-imc");
-
-    var valPeso = true;
-    var valAltura = true;
-
-    if(peso <= 0 || peso >= 1000) {
-        valPeso = false;
-        trIMC.textContent = "Peso inválido"
-        trPaciente.classList.add("paciente-invalido");
-    }
-
-    if(altura <= 0 || altura >= 3.0) {
-        valPeso = false;
-        trIMC.textContent = "Altura iválida";
-        trPaciente.classList.add("paciente-invalido");
-    }
-
-    if(valPeso && valAltura){
-        trIMC.textContent = (peso / (altura * altura)).toFixed(2);
-    }
-
-}
-
 var botao = document.querySelector("#adicionar-paciente");
 botao.addEventListener("click", function(event){
     event.preventDefault();
